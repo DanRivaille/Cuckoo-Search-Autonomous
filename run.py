029afa6ed61d5a8fb473cbbe7ea0db01b4cd26f1
DanRivaille <dan.santos.hd@outlook.com>
2021-10-13 20:31:35 -0300

    Se ordena el codigo del main

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
 from cso import CSO
 from cec2013lsgo.cec2013 import Benchmark
 
-bench = Benchmark()
 
-num_function = 2
+def main():
+    bench = Benchmark()
 
-info = bench.get_info(num_function)
-Lower = info['lower']
-Upper = info['upper']
-D = info['dimension']
-NP = 30
-pa = 0.25
-beta = 1.5
-N_Gen = 1500
-ObjetiveFunction = bench.get_function(num_function)
+    num_function = 2
 
-cuckoo = CSO(ObjetiveFunction, NP, D, pa, beta, Lower, Upper, N_Gen)
+    info = bench.get_info(num_function)
+    Lower = info['lower']
+    Upper = info['upper']
+    D = info['dimension']
+    NP = 30
+    pa = 0.25
+    beta = 1.5
+    N_Gen = 1500
+    ObjetiveFunction = bench.get_function(num_function)
 
-cuckoo.execute()
+    cuckoo = CSO(ObjetiveFunction, NP, D, pa, beta, Lower, Upper, N_Gen)
+    cuckoo.execute()
 
+
+def handle_args():
+    pass
+
+
+if __name__ == '__main__':
+    # Argument handling
+    handle_args()
+
+    main()
