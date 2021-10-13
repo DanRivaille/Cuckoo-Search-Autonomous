2a2ed9987efb4bbae9b9fe0bcba363c7b613a221
DanRivaille <dan.santos.hd@outlook.com>
2021-10-13 20:05:13 -0300

    Se agregan los archivos fuente del programa principal

---
+++ run.py
@@ -0,0 +1,22 @@
+from cso import CSO
+from cec2013lsgo.cec2013 import Benchmark
+from fitness import fitness_1
+
+bench = Benchmark()
+
+num_function = 2
+
+info = bench.get_info(num_function)
+Lower = info['lower']
+Upper = info['upper']
+D = info['dimension']
+NP = 30
+pa = 0.25
+beta = 1.5
+N_Gen = 1500
+ObjetiveFunction = bench.get_function(num_function)
+
+cuckoo = CSO(ObjetiveFunction, NP, D, pa, beta, Lower, Upper, N_Gen)
+
+cuckoo.execute()
+
