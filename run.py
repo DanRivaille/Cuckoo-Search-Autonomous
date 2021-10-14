17108e25f04a034cada4a507ffa0644cbae964b0
DanRivaille <dan.santos.hd@outlook.com>
2021-10-13 21:11:45 -0300

    Se adapta el main para que guarda los logs de las funciones

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ def main():
     bench = Benchmark()
 
     num_function = 2
+    ejecution = 0
 
     info = bench.get_info(num_function)
     BKS = info['best']
@@ -18,8 +19,8 @@ def main():
     N_Gen = 1500
     ObjetiveFunction = bench.get_function(num_function)
 
-    cuckoo = CSO(ObjetiveFunction, NP, D, pa, beta, Lower, Upper, N_Gen, 0, BKS)
-    cuckoo.execute(num_function)
+    cuckoo = CSO(ObjetiveFunction, NP, D, pa, beta, Lower, Upper, N_Gen, ejecution, BKS)
+    cuckoo.execute(num_function, f'Logs/function{num_function}_{ejecution}.csv')
 
 
 def handle_args():
